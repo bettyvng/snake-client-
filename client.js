58a6cab55c4dea3468642e59be992c99c881fa6c
bettyvng <betty.vg8@gmail.com>
2021-12-02 15:19:30 +0000

    makes changes to client and play. implement input

--- client.js
+++ client.js
@@ -9,15 +9,16 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log("Successfully connected to game server!");
     conn.write('Name: BZZ');
   });
-  
+
   conn.on('data', (data) => {
     console.log(data);
-  })
+  });
   return conn;
 };
 
