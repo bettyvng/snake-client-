c9ce251e4d5a081e0dceec53e44db4a3f21a8828
bettyvng <betty.vg8@gmail.com>
2021-12-02 15:56:24 +0000

    add movements

--- input.js
+++ input.js
@@ -1,17 +1,25 @@
-// setup interface to handle user input from stdin
-const setupInput = function () {
+let connection;
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.write('data', handleUserInput);
+  
   return stdin;
 };
 
 const handleUserInput = function(key) {
-    // \u0003 maps to ctrl+c input
-    if (key === '\u0003') {
-      process.exit();
-    }
+  switch (key)  {
+    case '\u0003': process.exit();
+    case 'w': connection.write('Move: up');
+      break;
+    case 'a': connection.write('Move: left');
+      break;
+    case 'd': connection.write('Move:right');
+      break;
+    case 's': connection.write('Move: down');
+      break;   
+    
   }
+}
   module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,10 +1,9 @@
-const net = require("net");
-const connect = require("./client");
-const setupInput = require("./input");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+let connectResult = connect();
+
+setupInput(connectResult);
 
 
